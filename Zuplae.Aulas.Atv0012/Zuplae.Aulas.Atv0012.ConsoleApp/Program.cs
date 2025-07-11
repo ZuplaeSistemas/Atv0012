41c04126a3609f14710f3d5d172e5f202bd834da
Maykon Granemann <maykondgranemann@gmail.com>
2025-07-10 21:19:15 -0300

    Aula Arquitetura em camadas

--- Zuplae.Aulas.Atv0012/Zuplae.Aulas.Atv0012.ConsoleApp/Program.cs
+++ Zuplae.Aulas.Atv0012/Zuplae.Aulas.Atv0012.ConsoleApp/Program.cs
@@ -1,50 +1,35 @@
-﻿
-using Zuplae.Aulas.Atv0012.Models;
+﻿using Zuplae.Aulas.Atv0012.ConsoleApp;
 
-Console.WriteLine("Atividade C# 0012");
+Console.WriteLine("=========  Sistema de cadatro da Atividade 0012 =========");
 
-Endereco endereco1 = new Endereco();
-endereco1.SetLogradouro("Rua das Flores");
-endereco1.SetNumero("123");
-endereco1.SetComplemento("Apto 45");
-endereco1.SetBairro("Jardim das Rosas");
-endereco1.SetCidade("São Paulo");
-endereco1.SetEstado("SP");
-endereco1.SetCep("01234-567");
+Console.WriteLine("Escolha uma das opções do menu abaixo: ");
+Console.WriteLine("\t1-Endereço");
+Console.WriteLine("\t2-Fornedor");
+Console.WriteLine("\t3-Produto");
+Console.WriteLine("\t4-Sair");
 
-Endereco endereco2 = new Endereco();
-endereco2.SetLogradouro("Avenida das Árvores");
-endereco2.SetNumero("456");
-endereco2.SetComplemento("Sala 12");
-endereco2.SetBairro("Bosque Encantado");
-endereco2.SetCidade("Rio de Janeiro");
-endereco2.SetEstado("RJ");
-endereco2.SetCep("98765-432");
+Console.Write("Digite a opção: ");
+int opcao = Convert.ToInt32( Console.ReadLine() );
 
-Fornecedor fornecedor1 = new Fornecedor();
-fornecedor1.SetRazaoSocial("Fornecedor A Ltda");
-fornecedor1.SetCnpj("12.345.678/0001-90");
-fornecedor1.SetEndereco(endereco1);
-
-Fornecedor fornecedor2 = new Fornecedor();
-fornecedor2.SetRazaoSocial("Fornecedor B S.A.");
-fornecedor2.SetCnpj("98.765.432/0001-01");
-fornecedor2.SetEndereco(endereco2);
-
-
-Produto produto1 = new Produto();
-produto1.SetNome("Geladeira");
-produto1.SetPreco(29.99m);
-produto1.SetFornecedor(fornecedor1);
-
-Produto produto2 = new Produto("Fritadeira", 49.99m, fornecedor2);
-
-
-Console.WriteLine(produto1);
-Console.WriteLine(produto2);
-
-Console.WriteLine($"{produto1.GetNome()} - {produto1.GetPreco():C}");
-Console.WriteLine($"{produto2.GetNome()} - {produto2.GetPreco():C}");
-
-
-
+switch (opcao)
+{
+	case 1:
+		TelaEndereco telaEndereco = new TelaEndereco();
+        telaEndereco.ExecutarMenu();
+        break;
+	case 2:
+		Console.WriteLine("Você escolheu Fornecedor.");
+		// Aqui você pode adicionar a lógica para manipular Fornecedor
+	break;
+	case 3:
+		Console.WriteLine("Você escolheu Produto.");
+        // Aqui você pode adicionar a lógica para manipular Produto
+        break;
+    case 4:
+        Console.WriteLine("Você escolheu Sair.");
+        // Aqui você pode adicionar a lógica para manipular Sair
+        break;
+    default:
+        Console.WriteLine("Opção Inválida");
+		break;
+}
